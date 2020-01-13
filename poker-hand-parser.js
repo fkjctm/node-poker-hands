722f4d27d834e2cbae3eed0e159289e146869fcb
Clint McElroy <clintmcelroy@agvance.net>
2020-01-13 16:26:18 -0600

    wip: straight flush done

--- poker-hand-parser.js
+++ poker-hand-parser.js
@@ -1,5 +1,24 @@
-const pokerHandParser = (hand) => {
-  return 'Royal Flush, Clubs';
+
+const getFlushSuit = (hand) => {
+  throw new Error('wip');
+};
+const getStraightHighCard = () => {
+  throw new Error('wip');
+};
+
+
+const pokerHandParser = (hand, getFlushSuitFn, getStraightHighCardFn) => {
+  const flushSuit = (getFlushSuitFn || getFlushSuit)(hand);
+  const straightHighCard = (getStraightHighCardFn || getStraightHighCard)(hand);
+
+  if(flushSuit !== null && straightHighCard === 'Ace') {
+    return `Royal Flush, ${flushSuit}`;
+  }
+  if(flushSuit !== null && straightHighCard !== 'Ace') {
+    return `Straight Flush, ${flushSuit}, ${straightHighCard} High`;
+  }
+
+  throw new Error('wip');
 };
 
 module.exports = pokerHandParser;
--- poker-hand-parser.test.js
+++ poker-hand-parser.test.js
@@ -3,6 +3,19 @@ const pokerHandParser = require('./poker-hand-parser');
 describe('pokerHandParser', () => {
 
   test('royal flush', () => {
-    expect(pokerHandParser('Tc Jc Qc Kc Ac')).toBe('Royal Flush, Clubs');
+    const getFlush = jest.fn().mockReturnValue('Clubs');
+    const getStraight = jest.fn().mockReturnValue('Ace');
+    const hand = 'Tc Jc Qc Kc Ac';
+
+    expect(pokerHandParser(hand, getFlush, getStraight)).toBe('Royal Flush, Clubs');
+    expect(getFlush).toBeCalledWith(hand);
+  });
+
+  test('straight flush', () => {
+    const getFlush = jest.fn().mockReturnValue('Clubs');
+    const getStraight = jest.fn().mockReturnValue('King');
+    const hand = '9c Tc Jc Qc Kc';
+
+    expect(pokerHandParser(hand, getFlush, getStraight)).toBe('Straight Flush, Clubs, King High');
   });
 });
