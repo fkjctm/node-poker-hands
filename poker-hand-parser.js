87733c99bcef2705e9843a881472d5e65610d8c2
Clint McElroy <clintmcelroy@agvance.net>
2020-01-13 18:37:08 -0600

    Top Level Testing
    Parser testing done with all abstract dependencies

--- poker-hand-parser.js
+++ poker-hand-parser.js
@@ -1,24 +1,88 @@
 
-const getFlushSuit = (hand) => {
+const handTransformer = () => {
+  throw new Error('wip');
+};
+const getFlushSuit = () => {
   throw new Error('wip');
 };
 const getStraightHighCard = () => {
   throw new Error('wip');
 };
+const getFourOfKindRank = () => {
+  throw new Error('wip');
+};
+const getFullHouseRanks = () => {
+  throw new Error('wip');
+};
+const getFlushRank = () => {
+  throw new Error('wip');
+};
+const getThreeOfKindRank = () => {
+  throw new Error('wip');
+};
+const getTwoPairRanks = () => {
+  throw new Error('wip');
+};
+const getOnePairRank = () => {
+  throw new Error('wip');
+};
+const getHighCardRank = () => {
+  throw new Error('wip');
+};
 
-
-const pokerHandParser = (hand, getFlushSuitFn, getStraightHighCardFn) => {
-  const flushSuit = (getFlushSuitFn || getFlushSuit)(hand);
-  const straightHighCard = (getStraightHighCardFn || getStraightHighCard)(hand);
+const pokerHandParser = (hand, helperFns) => {
+  const helpers = helperFns || {
+    handTransformer, getFlushSuit, getStraightHighCard, getFourOfKindRank, getFullHouseRanks,
+    getFlushRank, getThreeOfKindRank, getTwoPairRanks, getOnePairRank, getHighCardRank
+  };
+  const transformedHand = helpers.handTransformer(hand);
+  const flushSuit = helpers.getFlushSuit(transformedHand);
+  const straightHighCard = helpers.getStraightHighCard(transformedHand);
+  const fourOfKindRank = helpers.getFourOfKindRank(transformedHand);
+  const fullHouseRanks = helpers.getFullHouseRanks(transformedHand);
+  const flushRank = helpers.getFlushRank(transformedHand);
+  const threeOfKindRank = helpers.getThreeOfKindRank(transformedHand);
+  const twoPairRanks = helpers.getTwoPairRanks(transformedHand);
+  const onePairRank = helpers.getOnePairRank(transformedHand);
+  const highCardRank = helpers.getHighCardRank(transformedHand);
 
   if(flushSuit !== null && straightHighCard === 'Ace') {
-    return `Royal Flush, ${flushSuit}`;
-  }
-  if(flushSuit !== null && straightHighCard !== 'Ace') {
-    return `Straight Flush, ${flushSuit}, ${straightHighCard} High`;
+    return `Royal Flush: ${flushSuit}`;
   }
 
-  throw new Error('wip');
+  if(flushSuit !== null && straightHighCard !== null) {
+    return `Straight Flush: ${flushSuit}, ${straightHighCard} High`;
+  }
+
+  if(fourOfKindRank) {
+    return `Four of a Kind: ${fourOfKindRank}`;
+  }
+
+  if(fullHouseRanks) {
+    return `Full House: ${fullHouseRanks}`;
+  }
+
+  if(flushSuit) {
+    return `Flush: ${flushSuit}, ${flushRank} High`;
+  }
+
+  if(straightHighCard) {
+    return `Straight: ${straightHighCard} High`;
+  }
+
+  if(threeOfKindRank) {
+    return `Three of a Kind: ${threeOfKindRank}`;
+  }
+
+  if(twoPairRanks) {
+    return `Two Pair: ${twoPairRanks}`;
+  }
+
+  if(onePairRank) {
+    return `One Pair: ${onePairRank}`;
+  }
+
+  return `High Card: ${highCardRank}`;
 };
 
 module.exports = pokerHandParser;
